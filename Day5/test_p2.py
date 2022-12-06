31507d7b34aeda16d53b64850e4d7600b6152cd3
Rayane BOUDJEMAA <98220287+Mogulzz@users.noreply.github.com>
2022-12-06 23:13:28 +0100

    Create test_p2.py

---
+++ Day5/test_p2.py
@@ -0,0 +1,17 @@
+import re
+import os
+
+char = [ ['F','T','N','Z','M','G','H','J'], ['J','W','V'],  ['H','T','B','J','L','V','G'],  ['L','V','D','C','N','J','P','B'], ['G','R','P','M','S','W','F'], ['M','V','N','B','F','C','H','G'],  ['R','M','G','H','D'],  ['D','Z','V','M','N','H'], ['H','F','N','G'] ]
+with open('test.txt', 'r', encoding="utf-8") as f:
+    lines = f.readlines()
+    tab=[]
+    for line in lines:
+        x = re.findall('[0-9]+', line)
+        tab.append(x)
+        if (len(tab)==1):
+            char[int(tab[0][2])-1]= list((char[int(tab[0][1])-1][:int(tab[0][0])]))+char[int(tab[0][2])-1]
+            del char[int(tab[0][1])-1][:int(tab[0][0])]
+            tab.clear()
+    print('\n')
+    print("############################################################################ PART 1 ############################################################################")
+    print("Answer Part 2:", char[0][0],char[1][0],char[2][0],char[3][0],char[4][0],char[5][0],char[6][0],char[7][0],char[8][0])
