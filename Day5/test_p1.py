28b8228b827ea51b36f516b437bb170cea8beeb1
Rayane BOUDJEMAA <98220287+Mogulzz@users.noreply.github.com>
2022-12-06 23:02:29 +0100

    Create test_p1.py

---
+++ Day5/test_p1.py
@@ -0,0 +1,15 @@
+char2 = [ ['F','T','N','Z','M','G','H','J'], ['J','W','V'],  ['H','T','B','J','L','V','G'],  ['L','V','D','C','N','J','P','B'], ['G','R','P','M','S','W','F'], ['M','V','N','B','F','C','H','G'],  ['R','M','G','H','D'],  ['D','Z','V','M','N','H'], ['H','F','N','G'] ]
+char = [ ['N','Z'], ['D','C','M'],  ['P']]
+with open('test2.txt', 'r', encoding="utf-8") as f:
+    lines = f.readlines()
+    tab=[]
+    for line in lines:
+        x = re.findall('[0-9]+', line)
+        tab.append(x)
+        if (len(tab)==1):
+            char[int(tab[0][2])-1]= list(reversed(char[int(tab[0][1])-1][:int(tab[0][0])]))+char[int(tab[0][2])-1]
+            del char[int(tab[0][1])-1][:int(tab[0][0])]
+            tab.clear()
+    print('\n')
+    print("############################################################################ PART 1 ############################################################################")
+    print("Answer Part 1:", char)
